5d957e69ad1b1c687b9395af5c16dbd2d4d8620c
peter-howell <44275653+peter-howell@users.noreply.github.com>
2025-02-15 12:57:55 -0500

    run without selection and move to next line after running

--- src/runcode.ts
+++ src/runcode.ts
@@ -15,6 +15,11 @@ export async function runText(text: string | undefined): Promise<void> {
     if (!term || !text) {
         return;
     }
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return;
+    }
+    const sel0 = editor.selections;
 
     const split = text.split('\n');
     const last_split = split.length - 1;
@@ -22,13 +27,15 @@ export async function runText(text: string | undefined): Promise<void> {
         if (count > 0) {
             await delay(80); // Increase delay if it can't handle speed.
         }
-        if (count === last_split) {
+        if (count === last_split && count > 0) {
             // Avoid sending newline on last line
             term.sendText(line, false);
         } else term.sendText(line);
     }
-    term.show(false)
+    term.show(true);
     // Scroll console to see latest output
     await vscode.commands.executeCommand('workbench.action.terminal.scrollToBottom');
+    editor.selections = sel0;
+    await vscode.window.showInformationMessage('THING HAPPENED');
     
 }
--- src/utils.ts
+++ src/utils.ts
@@ -12,15 +12,23 @@ export async function getSelection(): Promise<string | undefined> {
     if (!editor) {
         return;
     }
+    let linesDownToMoveCursor = 0;
     const selection = editor.selection;
     const start = selection.start;
+    let newStart = new Position(start.line, 0);
+    let newEnd = new Position(start.line, 0);
     if (selection.isEmpty) {
-        const charactersOnLine = editor.document.lineAt(selection.start.line).text.length;
-        const newStart = new Position(start.line, 0);
-        const newEnd = new Position(start.line, charactersOnLine);
-        return editor.document.getText(new Range(newStart, newEnd));
+        const charactersOnLine = editor.document.lineAt(start.line).text.length;
+        newEnd = new Position(start.line, charactersOnLine);
+        linesDownToMoveCursor = 1;
+    } else {
+        newStart = start;
+        newEnd = selection.end;
+        linesDownToMoveCursor = selection.end.line - selection.start.line + 1;
     }
-    return editor.document.getText(selection);    
+    await vscode.commands.executeCommand('cursorMove', { to: 'down', value: linesDownToMoveCursor });
+    await vscode.commands.executeCommand('cursorMove', { to: 'wrappedLineFirstNonWhitespaceCharacter' });
+    return editor.document.getText(new Range(newStart, newEnd));  
 }
 
 export async function getClasspath(): Promise<string> {
